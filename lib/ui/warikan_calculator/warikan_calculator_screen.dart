5fe6ccd79eff9b3daa8e040df8a44641e28a4e7b
momomo0420-lab <momomo0420.lab+github@gmail.com>
2023-10-11 22:59:36 +0900

    Completed to create the Warikan calculator.

--- lib/ui/warikan_calculator/warikan_calculator_screen.dart
+++ lib/ui/warikan_calculator/warikan_calculator_screen.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:warikan_calculator/ui/warikan_calculator/warikan_calculator_body.dart';
+import 'package:warikan_calculator/ui/warikan_calculator/warikan_calculator_viewmodel.dart';
 
 class WarikanCalculatorScreen extends ConsumerWidget {
   const WarikanCalculatorScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // ビューモデル
+    final viewModel = ref.watch(warikanCalculatorViewModelProvider.notifier);
+    // 状態
+    final state = ref.watch(warikanCalculatorViewModelProvider);
+
     return Scaffold(
       appBar: AppBar(title: const Text('割り勘金額計算アプリ')),
-      body: const WarikanCalculatorBody(),
+      body: WarikanCalculatorBody(
+        viewModel: viewModel,
+        state: state
+      ),
     );
   }
 }
--- lib/ui/warikan_calculator/warikan_calculator_viewmodel.dart
+++ lib/ui/warikan_calculator/warikan_calculator_viewmodel.dart
@@ -4,6 +4,7 @@ import 'package:warikan_calculator/ui/warikan_calculator/warikan_calculator_view
 
 part 'warikan_calculator_viewmodel.g.dart';
 
+/// WarikanCalculatorのビューモデル
 @riverpod
 class WarikanCalculatorViewModel extends _$WarikanCalculatorViewModel {
   /// 初期化処理
@@ -13,7 +14,7 @@ class WarikanCalculatorViewModel extends _$WarikanCalculatorViewModel {
       amountController: TextEditingController(),
       taxRateController: TextEditingController(),
       numberController: TextEditingController(),
-      isTaxIncluded: true,
+      isTaxIncluded: false,
     );
   }
 
@@ -22,5 +23,58 @@ class WarikanCalculatorViewModel extends _$WarikanCalculatorViewModel {
   /// [taxIncluded]を基に税率の有無を設定する
   void setTaxIncluded(bool taxIncluded) {
     state = state.copyWith(isTaxIncluded: taxIncluded);
+  }
+
+  void clearAmountController() {
+    state.amountController.clear();
+  }
+
+  void clearTaxRateController() {
+    state.taxRateController.clear();
+  }
+
+  void clearNumberController() {
+    state.numberController.clear();
+  }
+
+  /// 必要なすべての項目の入力が終わっているか判定する
+  bool _checkAllFieldEntered() {
+    var result = true;
+
+    // 金額と人数が入力されていない場合は無効
+    if((state.amountController.text == "") ||
+        (state.numberController.text == "")) {
+      result = false;
+    }
+
+    // 税別価格なのに税率が入力されていない場合は無効
+    if(!state.isTaxIncluded && (state.taxRateController.text == "")) {
+      result = false;
+    }
+
+    return result;
+  }
+
+  /// 割り勘金額を計算する
+  void calculateAmountPerPerson(
+    Function(double result)? onSuccess,
+    Function()? onFailure
+  ) {
+    if(!_checkAllFieldEntered()) {
+      if(onFailure != null) onFailure();
+      return;
+    }
+
+    var amount = double.parse(state.amountController.text);
+
+    if(!state.isTaxIncluded) {
+      final taxRate = int.parse(state.taxRateController.text);
+      final tax = amount * taxRate / 100;
+      amount += tax;
+    }
+
+    final number = int.parse(state.numberController.text);
+
+    if(onSuccess != null) onSuccess(amount / number);
   }
 }
